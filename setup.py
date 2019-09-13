fe89fb45a1c0dacaee9177da27d8b4e7a9101268
ttimbers <tiffany.timbers@stat.ubc.ca>
2019-09-12 21:29:38 -0700

    made into a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='rudaux',
+      version='0.1',
+      description='functions for working with canvas courses in the context of dsci-100 infrastructure',
+      url='http://github.com/UBC-DSCI/rudaux2',
+      author='Tiffany Timbers',
+      author_email='tiffany.timbers@gmail.com',
+      license='MIT',
+      packages=['rudaux2'],
+      zip_safe=False)

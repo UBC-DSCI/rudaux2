3a02551fabe5203f102f7b448be11a87fad56def
Tiffany A. Timbers <tiffany.timbers@gmail.com>
2019-09-18 21:33:52 -0700

    made use of optional args clearer

--- src/return_grades.py
+++ src/return_grades.py
@@ -11,9 +11,11 @@
 # point_adjust which is useful if you forget to assign points to a question.
 #
 # Example:
+# python return_grades.py --assignment=tutorial_01 --grades_path
 #
+# This script can also adjust the grades in case a mistake was made assigning points. 
+# This can be done using the point_adjust optional argument:
 # python return_grades.py --assignment=tutorial_01 --grades_path --point_adjust=3
-
 import requests
 import os
 import urllib.parse
@@ -27,15 +29,16 @@ parser.add_argument('--assignment')
 parser.add_argument('--grades_path')
 parser.add_argument('--point_adjust')
 args = parser.parse_args()
-point_adjust = args.point_adjust
 assignment = args.assignment
+grades_path = args.grades_path
+point_adjust = args.point_adjust
 
 # course settings (eventually abtract to a config file)
 dsci100_canvasHostName = 'https://canvas.ubc.ca'
 dsci100_courseID = '40616'
 
 # get student id & grades as percentage
-nbgrader_grades = pd.read_csv(args.grades_path)
+nbgrader_grades = pd.read_csv(grades_path)
 nbgrader_grades = nbgrader_grades.query('assignment == @assignment')
 if point_adjust is not None:
     nbgrader_grades['max_score'] = nbgrader_grades['max_score'] + int(point_adjust)
@@ -45,4 +48,4 @@ nbgrader_grades.to_csv(assignment + '-grades.csv', index=False)
 
 # post grades to Canvas
 dsci100 = rudaux2.course(dsci100_canvasHostName, dsci100_courseID) # get these values 
-dsci100.post_grades(nbgrader_grades, args.assignment)
+dsci100.post_grades(nbgrader_grades, assignment)
